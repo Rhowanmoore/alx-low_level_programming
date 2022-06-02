5c66ba95a3aa4ccf0ecee6249bc937d5010a8b13
rhowan <Rhowanmoore@gmail.com>
2022-06-02 10:03:06 -0700

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,23 @@
-#include <stddio.h>
+nclude <stdio.h>
+
+
 
 /**
- *  *  * main - Entry point
- *   *   *
- *    *   *Return: Always 0 (Success)
- *     **/
+ *
+ * * main - Entry point
+ *
+ *  * puts message
+ *
+ *   * Return: 0
+ *
+ *    */
+
 int main(void)
+
 {
-	        puts(*\"Programming is like building a multilingual puzzle");
-		        return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
